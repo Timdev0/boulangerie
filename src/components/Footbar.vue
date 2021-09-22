101dea4397f2edec3d10494af4dc4b5909396e9b
Timdev0 <quentin.peltier.pro@gmail.com>
2021-09-22 15:11:58 +0200

    add base views and components

---
+++ src/components/Footbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="footbar-component">
+        <p>Made with music and coffee</p>
+    </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="navbar-component">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/doc">Doc</router-link>
+    </div>
+</template>
+
+<style scoped>
+    #navbar-component {
+    padding: 30px;
+    }
+
+    #navbar-component a {
+    font-weight: bold;
+    color: #2c3e50;
+    }
+
+    #navbar-component a.router-link-exact-active {
+    color: #42b983;
+    }
+</style>
